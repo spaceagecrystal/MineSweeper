13a62bde2da60101e36b343fa69669f8b4a428ce
Mitch Greer <mitch@skymachine.com>
2018-04-30 10:42:35 -0700

    part 2

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,56 @@
+let getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+
+  let neigborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 0],
+    [0, 1],
+    [-1, 1],
+    [1, 0],
+    [1, 1]
+  ];
+
+  const numberOfRows = bombBoard.length();
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neigborOffsets.forEach(offset => {
+
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+
+    if (neighborRowIndex >= numberOfRows &&
+      neighborRowIndex < numberOfRows &&
+      neighborColumnIndex >= 0 &&
+      neighborColumnIndex < columnIndex)
+
+    {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] != 'B') {
+        numberOfBombs++
+
+      }
+
+    }
+
+  });
+  return numberOfBombs;
+}
+const flipTile = (playerBoard, bombBoard, rowIndex,columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] != ' '){
+    console.log('This tile has already been flipped.')
+    return;
+  } else if (playerBoard[rowIndex][columnIndex] = 'B') {
+
+
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+
+  }
+
+}
+
+
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
   for (let rowCount = 0; rowCount < numberOfRows; rowCount++) {
@@ -24,12 +77,14 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
   while (numberOfBombsPlaced < numberOfBombs) {
     //ILl learn about control flow later.
-    let randomRowIndex = Math.random() * numberOfRows;
-    randomRowIndex = Math.floor(randomRowIndex);
-    let randomColumnIndex = Math.random() * numberOfColumns;
-    randomColumnIndex = Math.floor(randomColumnIndex);
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    if (board[randomRowIndex][randomColumnIndex] != 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
+
   }
   return board;
 }
@@ -42,3 +97,6 @@ console.log('Player Board: ')
 printBoard(playerBoard);
 console.log('Bomb Board:');
 printBoard(bombBoard);
+flipTile(playerBoard, bombBoard,0,0)
+console.log("Updated Player Board");
+printBoard(playerBoard);
